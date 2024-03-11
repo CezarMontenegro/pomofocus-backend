c4cca9237be0e904c4358aa6aa6193fa27481b01
Cezar <cezarguimaraes@gmail.com>
2024-03-11 08:51:55 -0300

    initiating devoloping the orm

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,22 @@ server.get('/', (req, res) => {
   res.send('Rodando');
 });
 
-server.listen(3333, () => {console.log('Rodando na porta 3333')});
+server.post('/users', (req, res) => {
+
+  const query = `INSERT INTO users (name, email, password) VALUES ('Cezar', 'cezarguimaraes@gmail.com', '123456');`;
+
+  conn.query(query, (err) => console.log(err));
+
+  res.send('Deu certo');
+})
+
+conn.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Database has been connected');
+  server.listen(3333, () => {console.log('Running on port: 3333')});
+})
+
 
